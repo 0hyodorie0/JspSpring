d94a1e6224189e1a4db330d11032f9d78c1bb18a
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-23 12:08:19 +0900

    edit servlet

--- workspace/webStudy01/src/kr/or/ddit/servlet01/DescriptionServlet.java
+++ workspace/webStudy01/src/kr/or/ddit/servlet01/DescriptionServlet.java
@@ -10,16 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 //Convention over Configuration
-@WebServlet()
+@WebServlet("/desc")
 public class DescriptionServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		MIME(MultiPurposed Internet Mail Extension)
+//		main_type/sub_type;charset=encoding
+//		ex) text/html, text/javascript, text/css, text/plain
+		
+		String mime = "text/html;charset=UTF-8";
+		resp.setContentType(mime);
 		String ip = req.getRemoteAddr();
 		StringBuffer html = new StringBuffer();
 		html.append("<html>                               \n");
 		html.append("	<body>                            \n");
-		html.append(String.format("		<h4>client ip address : %s</h4>\n", ip));
+		html.append(String.format("		<h4>클라이언트 ip address : %s</h4>\n", ip));
 		html.append("	</body>                           \n");
 		html.append("</html>                              \n");
 		PrintWriter out = resp.getWriter();
