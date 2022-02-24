233ce2a8c7cf3a203d2bc6f757426d4824225115
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-24 10:26:33 +0900

    Update GugudanServlet.java

--- workspace/webStudy01/src/kr/or/ddit/servlet02/GugudanServlet.java
+++ workspace/webStudy01/src/kr/or/ddit/servlet02/GugudanServlet.java
@@ -1,5 +1,19 @@
 package kr.or.ddit.servlet02;
+<<<<<<< Updated upstream
 
 public class GugudanServlet {
 
+=======
+/**
+ * 1. /02/gugudan.do 요청이 발생하면,
+ * 2. 2단부터 9단까지 승수(1~9)에 따라 구구단 컨텐츠 제공.
+ * 3. table 태그 UI
+ * 4. 한 단이 하나의 행을 구성함.
+ * 5. 오늘, 현재 시각이 h4 태그를 이용한 타이틀로 출력됨.
+ * 6. 요청은 get 방식으로 발생함.
+ *
+ */
+public class GugudanServlet {
+	
+>>>>>>> Stashed changes
 }
